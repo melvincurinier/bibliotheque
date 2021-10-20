d42c7d175a07ce4d2e0963319dccecc84b93fa34
melvincurinier <melvin.curinier@etu.univ-lyon1.fr>
2021-10-20 09:34:26 +0200

    initial commit

---
+++ formulaire_abonne.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Formulaire pour les abonnées</title>
+</head>
+<body>
+    <?php
+        echo'
+        <header>
+            <div class="menuBar">
+                <ul>
+                    <li>Abonné</li>
+                    <li>Livre</li>
+                    <li>Emprunt</li>
+                </ul>
+            </div>
+        </header>
+        ';
+    ?>
+</body>
+</html>
